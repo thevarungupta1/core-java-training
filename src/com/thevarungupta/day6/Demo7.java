13f91fd236fcf4e66c1afd529d3f02bcba04c2de
Varun Gupta <thevarungupta5@gmail.com>
2021-04-19 10:41:23 +0530

    threading

---
+++ src/com/thevarungupta/day6/Demo7.java
@@ -0,0 +1,75 @@
+package com.thevarungupta.day6;
+
+/**
+ * Synchronization in Java
+ * It is a capability to control the access of multiple threads to any share resource
+ * It give us better option where we want to allow only one thread to access te shared resource
+ *
+ * # Why use synchronization
+ * 1. to prevent thread interface
+ * 2. to prevent consistency problem
+ *
+ * # Type of Synchronization
+ * 1. Process Synchronization
+ * 2. Thread Synchronization
+ *
+ * */
+
+class Table{
+    // Without Synchronization
+//    public void printTable(int number){
+//        for(int i=0;i<10;i++){
+//            System.out.println(number + " x "+ i + " = "+ number *i);
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+    // with Synchronization
+    synchronized void printTable(int number){
+        for(int i=0;i<10;i++){
+            System.out.println(number + " x "+ i + " = "+ number *i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class MyThread1 extends Thread{
+    Table table;
+    MyThread1(Table table){
+        this.table = table;
+    }
+
+    @Override
+    public void run() {
+        table.printTable(5);
+    }
+}
+
+class MyThread2 extends Thread{
+    Table table;
+    MyThread2(Table table){
+        this.table = table;
+    }
+
+    @Override
+    public void run() {
+        table.printTable(100);
+    }
+}
+
+public class Demo7 {
+    public static void main(String[] args) {
+        Table tableObject = new Table();
+        MyThread1 t1 = new MyThread1(tableObject);
+        MyThread2 t2 = new MyThread2(tableObject);
+        t1.start();
+        t2.start();
+    }
+}
